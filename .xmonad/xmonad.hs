1d93c90ed5c746f6833bcc91719c50d71e2d3023
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2015-12-29 00:42:39 +0100

    refactor in constants
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,5 +1,6 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module Main(main) where
-
+import           Text.Printf                    (printf)
 import           XMonad
 import qualified XMonad.Config.ZsEdem           as ZsEdem
 import           XMonad.Hooks.ManageDocks       (avoidStruts, docksEventHook,
@@ -10,22 +11,21 @@ import           XMonad.Layout.WindowNavigation (windowNavigation)
 import           XMonad.Util.EZConfig           (additionalKeys)
 import           XMonad.Util.Run                (spawnPipe)
 
-hPath = "/home/zsedem/"
+clr1,clr2,clr3 :: String
 clr1 = "#efefef"
 clr2 = "#000000"
 clr3 = "#0783c0"
-sWidth  = "1600"
-sHeight = "900"
+sWidth :: Int
+sWidth  = 1600
 
-layoutHook' = avoidStruts  $ smartBorders $ windowNavigation ( sTall ||| Mirror mTall ||| Full )
+layoutHook' = avoidStruts $ smartBorders $ windowNavigation ( sTall ||| Mirror mTall ||| Full )
         where
             sTall = spacing 10 $ Tall 1 (3/100) (2/3)
             mTall = spacing 10 $ Tall 1 (3/100) (2/3)
 
 main = do
-    bgPanel <- spawnPipe bgBar
+    spawn iBar
     layoutPanel <- spawnPipe lBar
-    infoPanel <- spawnPipe iBar
     workspacePanel <- spawnPipe wBar
     xmonad $ defaultConfig
      { manageHook = ZsEdem.manageHook <+> manageDocks <+> manageHook defaultConfig
@@ -44,8 +44,13 @@ main = do
      } `additionalKeys` ZsEdem.keyBindings
      where
         dzenArgs = "-p -e 'button3=' -fn 'Droid Sans Fallback-8:bold'"
-        bgBar    = "echo '^fg("++clr3++")^p(;+20)^r(1600x5)' | dzen2 " ++ dzenArgs++" -ta c -fg '" ++ clr1 ++ "' -bg '#000000' -h 35 -w "++sWidth
-        lBar     = "sleep 0.3;exec dzen2 "++dzenArgs++" -ta l -fg '"++clr1++"' -bg '" ++clr2++"' -h 25 -w `expr "++sWidth++" / 2` -y 0"
-        iBar     = "sleep 0.3; conky -c ~/.xmonad/assets/conky/info.conkyrc | dzen2 "++dzenArgs++" -ta r -fg '"++clr1++"' -bg '" ++clr2++"' -h 25 -w `expr "++sWidth++" / 2` -x `expr "++sWidth++" / 2 - 50` -y 0"
-        wBar     = "sleep 0.6;exec dzen2 "++dzenArgs++" -ta c -fg '"++clr1++"' -bg '" ++clr2++"' -h 20 -w 300 -x `expr "++sWidth++" / 2 - 150` -y 4"
-
+        lBar     = printf
+                        "sleep 0.3;exec dzen2 %s -ta l -fg '%s' -bg '%s' -h 25 -w %d -y 0"
+                        dzenArgs clr1 clr2 (sWidth // 2)
+        iBar     = printf
+                        "sleep 0.3; conky -c ~/.xmonad/assets/conky/info.conkyrc | dzen2 %s -ta r -fg '%s' -bg '%s' -h 25 -w %d -x %d -y 0"
+                        dzenArgs clr1 clr2 (sWidth // 2) (sWidth // 2 - 50)
+        wBar     = printf
+                        "sleep 0.6;exec dzen2 %s -ta c -fg '%s' -bg '%s' -h 20 -w 300 -x %d -y 4"
+                        dzenArgs clr1 clr2 (sWidth // 2 - 150)
+        (//)      = div
