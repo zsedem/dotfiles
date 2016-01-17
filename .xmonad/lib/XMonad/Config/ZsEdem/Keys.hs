d27a9dc7583c58a44183b188b14f5e32c856aa35
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2016-01-17 12:49:54 +0100

    refactoring some module so it will less username dependent
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

--- .xmonad/lib/XMonad/Config/ZsEdem/Keys.hs
+++ .xmonad/lib/XMonad/Config/ZsEdem/Keys.hs
@@ -4,10 +4,8 @@ module XMonad.Config.ZsEdem.Keys
   where
 import           Data.List(elemIndex)
 import           Graphics.X11.Types
-import           XMonad                         (KeyMask, X, controlMask, kill,
-                                                 mod1Mask, sendMessage,
-                                                 shiftMask, spawn, windows,
-                                                 (.|.))
+import           XMonad                         (X, kill, sendMessage,
+                                                 spawn, windows, (.|.))
 import           XMonad.Actions.WindowBringer   (bringMenuArgs', gotoMenuArgs')
 import           XMonad.Hooks.ManageDocks       (ToggleStruts (..))
 import qualified XMonad.Layout.WindowNavigation as W
@@ -29,14 +27,13 @@ windowsButton :: KeyMask
 windowsButton = mod4Mask
 
 
-keyBindings :: [((KeyMask, KeySym), X ())]
-keyBindings = [ ((mod4Mask              , xK_p                          ), loggedSpawn dmenu_run)
+keyBindings :: FilePath -> String -> String -> [((KeyMask, KeySym), X ())]
+keyBindings homePath foregroundColor themeColor =
+              [ ((mod4Mask              , xK_p                          ), loggedSpawn dmenu_run)
               , ((mod4Mask              , xK_q                          ), spawn xmonad_restart)
               , ((mod4Mask .|. shiftMask, xK_q                          ), loggedSpawn powermenu)
               , ((windowsButton         , xK_Tab                        ), windows S.focusDown )
               , ((shift windowsButton   , xK_Tab                        ), windows S.focusUp )
-              -- , ((shiftMask             , xK_Insert                     ), pasteSelection)
-
               , ((windowsButton         , xK_Right                      ), sendMessage $ W.Swap W.R )
               , ((windowsButton         , xK_Left                       ), sendMessage $ W.Swap W.L )
               , ((windowsButton         , xK_Up                         ), sendMessage $ W.Swap W.U )
@@ -44,8 +41,8 @@ keyBindings = [ ((mod4Mask              , xK_p                          ), logge
               , ((alt                   , xK_F4                         ), kill )
               , ((windowsButton         , xK_F4                         ), kill )
               , ((windowsButton         , xK_F11                        ), sendMessage ToggleStruts )
-              , ((alt                   , xK_Tab                        ), gotoMenuArgs' dmenu_run_file [sWidth, sHeight, clr2, clr3] )
-              , ((shift alt             , xK_Tab                        ), bringMenuArgs' dmenu_run_file [sWidth, sHeight, clr2, clr3] )
+              , ((alt                   , xK_Tab                        ), gotoMenuArgs' dmenu_run_file [sWidth, sHeight, foregroundColor, themeColor] )
+              , ((shift alt             , xK_Tab                        ), bringMenuArgs' dmenu_run_file [sWidth, sHeight, foregroundColor, themeColor] )
               , ((controlMask           , xK_space                      ), layoutSwitch)
               , ((windowsButton         , xK_l                          ), spawn "xscreensaver-command -lock")
               , ((0                     , xMediaButton_AudioRewind      ), spawn "amixer -q set Master toggle" )
@@ -54,10 +51,10 @@ keyBindings = [ ((mod4Mask              , xK_p                          ), logge
               , ((ctrl shiftMask        , xK_q                          ), return ())
               ]
              where
-                dmenu_run_file = hPath ++ ".xmonad/assets/bin/dmenu.sh"
-                dmenu_run = hPath ++ ".xmonad/assets/bin/dmenu-run.sh '"++sWidth ++"' '"++sHeight ++"' '"++clr2++"' '"++clr3++"'"
-                powermenu = hPath++".xmonad/assets/bin/powermenu.sh '"++sWidth ++"' '"++sHeight ++"' '"++clr2++"' '"++clr3++"'"
-                xmonad_restart = hPath++".xmonad/assets/bin/restart.sh"
+                dmenu_run_file = homePath ++ ".xmonad/assets/bin/dmenu.sh"
+                dmenu_run = homePath ++ ".xmonad/assets/bin/dmenu-run.sh '"++sWidth ++"' '"++sHeight ++"' '"++foregroundColor++"' '"++themeColor++"'"
+                powermenu = homePath++".xmonad/assets/bin/powermenu.sh '"++sWidth ++"' '"++sHeight ++"' '"++foregroundColor++"' '"++themeColor++"'"
+                xmonad_restart = homePath++".xmonad/assets/bin/restart.sh"
                 loggedSpawn c = spawn $ "echo '"++c++ "'>> /tmp/xmonad.spawn.log; " ++ c
                 layoutSwitch = do keyboardlayout <- R.runProcessWithInput "/usr/bin/xkblayout-state" ["print", "%s"] ""
                                   let newLayout = nextItem ["us", "hu"] keyboardlayout
@@ -73,9 +70,6 @@ keyBindings = [ ((mod4Mask              , xK_p                          ), logge
                 -- xMediaButton_Home             = 0x1008ff18
                 -- xMediaButton_Play             = 0x1008ff14
                 -- xMediaButton_MyComputer       = 0x1008ff5d
-                hPath = "/home/zsedem/"
-                clr2 = "#000000"
-                clr3 = "#0783c0"
                 sWidth  = "1600"
                 sHeight = "900"
 
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module Main(main) where
 import           Text.Printf                    (printf)
+import           System.Directory               (getHomeDirectory)
 import           XMonad
 import qualified XMonad.Config.ZsEdem           as ZsEdem
 import           XMonad.Hooks.ManageDocks       (avoidStruts, docksEventHook,
@@ -11,10 +12,10 @@ import           XMonad.Layout.WindowNavigation (windowNavigation)
 import           XMonad.Util.EZConfig           (additionalKeys)
 import           XMonad.Util.Run                (spawnPipe)
 
-clr1,clr2,clr3 :: String
-clr1 = "#efefef"
-clr2 = "#000000"
-clr3 = "#0783c0"
+backgroundColor,foregroundColor,themeColor :: String
+backgroundColor = "#efefef"
+foregroundColor = "#000000"
+themeColor = "#0783c0"
 sWidth :: Int
 sWidth  = 1600
 
@@ -24,6 +25,7 @@ layoutHook' = avoidStruts $ smartBorders $ windowNavigation ( Full ||| sTall |||
             mTall = spacing 5 $ Tall 1 (3/100) (2/3)
 
 main = do
+    homePath <- (++"/")<$> getHomeDirectory
     spawn iBar
     layoutPanel <- spawnPipe lBar
     workspacePanel <- spawnPipe wBar
@@ -35,22 +37,22 @@ main = do
      , focusFollowsMouse  = False
      , clickJustFocuses   = False
      , terminal = "urxvt"
-     , focusedBorderColor = clr3
+     , focusedBorderColor = themeColor
      , normalBorderColor = "#424242"
      , borderWidth = 2
      , startupHook = ZsEdem.startupHook
      , logHook = ZsEdem.layoutDzenLogHook layoutPanel <+> ZsEdem.workspaceDzenLogHook workspacePanel
-     , handleEventHook    = handleEventHook defaultConfig <+> docksEventHook
-     } `additionalKeys` ZsEdem.keyBindings
+     , handleEventHook = docksEventHook <+> handleEventHook defaultConfig
+     } `additionalKeys` ZsEdem.keyBindings homePath foregroundColor themeColor
      where
         dzenArgs = "-p -e 'button3=' -fn 'Droid Sans Fallback-8:bold'"
         lBar     = printf
                         "sleep 0.3;exec dzen2 %s -ta l -fg '%s' -bg '%s' -h 25 -w %d -y 0"
-                        dzenArgs clr1 clr2 (sWidth // 2)
+                        dzenArgs backgroundColor foregroundColor (sWidth // 2)
         iBar     = printf
                         "sleep 0.3; conky -c ~/.xmonad/assets/conky/info.conkyrc | dzen2 %s -ta r -fg '%s' -bg '%s' -h 25 -w %d -x %d -y 0"
-                        dzenArgs clr1 clr2 (sWidth // 2) (sWidth // 2 - 50)
+                        dzenArgs backgroundColor foregroundColor (sWidth // 2) (sWidth // 2 )
         wBar     = printf
                         "sleep 0.6;exec dzen2 %s -ta c -fg '%s' -bg '%s' -h 20 -w 300 -x %d -y 4"
-                        dzenArgs clr1 clr2 (sWidth // 2 - 150)
+                        dzenArgs backgroundColor foregroundColor (sWidth // 2 - 150)
         (//)      = div
