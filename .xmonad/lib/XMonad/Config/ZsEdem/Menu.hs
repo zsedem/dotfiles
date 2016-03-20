d46cb3bd34febafdcb10cdda8f1a8f3734bf48b7
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2016-03-20 13:39:20 +0100

    used yeganesh in the menu
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

--- .xmonad/lib/XMonad/Config/ZsEdem/Menu.hs
+++ .xmonad/lib/XMonad/Config/ZsEdem/Menu.hs
@@ -12,7 +12,6 @@ import Data.List(isSubsequenceOf, isPrefixOf)
 import Data.Char(toLower)
 import System.Directory(getDirectoryContents)
 import XMonad hiding(windows)
-import XMonad.Util.Dmenu(menuArgs)
 import XMonad.Util.NamedWindows(getName)
 import qualified XMonad.StackSet as W
 import XMonad.Util.Run(runProcessWithInput)
@@ -22,11 +21,8 @@ import XMonad.Config.ZsEdem.Core
 
 
 windowSwitchMenu, windowBringMenu :: X ()
-windowSwitchMenu = gotoMenuArgs' dmenuRunFile [sWidth, sHeight, foregroundColor, themeColor]
-windowBringMenu = bringMenuArgs' dmenuRunFile [sWidth, sHeight, foregroundColor, themeColor]
-
-dmenuBin :: String
-dmenuBin = ".xmonad/assets/bin/dmenu.sh"
+windowSwitchMenu = gotoMenuArgs' dmenuBin dmenuArgs
+windowBringMenu = bringMenuArgs' dmenuBin dmenuArgs
 
 queryIsInTitle :: String -> Query Bool
 queryIsInTitle windowNameMatcher = isSubsequenceOf windowNameMatcher <$> title
@@ -40,30 +36,36 @@ nextTitleMatchOrSpawn windowSubTitle command = nextMatchOrDo
                                                     (queryIsInTitle windowSubTitle)
                                                     (loggedSpawn command)
 
+menuArgs :: String -> [String] -> [String] -> X String
+menuArgs menuCmd args opts = filter (/='\n') <$> runProcessWithInput menuCmd args (unlines opts)
+
 customCommandMenu :: X ()
 customCommandMenu = do
     windows <- windowMap
     layoutFiles <- filter (`notElem` [".", ".."] ) <$> liftIO (getDirectoryContents ".screenlayout")
-    let commands = ["jira", "hangups", "mail", "inbox", "pycharm", "term", "calendar", "newtmux",
-                    "chrome", "xmonad config", "review", "network", "wifi", "jenkins",
+    let commands = ["hangups", "pycharm", "term", "newtmux", "wifi off",
+                    "chrome", "xmonad config", "review", "network", "wifi on",
                     "google", "openUrl"]
         windowPrefix = "window: "
         commandPrefix = "command: "
         screenLayoutPrefix = "screenlayout: "
+        webPrefix = "web: "
         (|-) = isPrefixOf
 
         selections = concat
             [ map (commandPrefix++) commands
             , map (fst>>>(windowPrefix++)) windows
             , map (screenLayoutPrefix++) layoutFiles
+            , map (fst>>>(webPrefix++)) webAppMap
             ]
-    choosed <- menuArgs dmenuBin args selections
+    choosed <- menuArgs dmenuBin dmenuArgs selections
+    spawn $ unwords ["echo '", choosed, "' >> .logs/yeganesh.log "]
     if  | "!" |- choosed -> loggedSpawn $ drop 1 choosed
         | windowPrefix |- choosed -> do
             let windowName = drop (length windowPrefix) choosed
             case lookup windowName windows of
                 Just window -> focus window
-                Nothing -> return ()
+                _ -> return ()
         | commandPrefix |- choosed -> do
             let command = drop (length commandPrefix) choosed
             commandRun command
@@ -71,25 +73,23 @@ customCommandMenu = do
             let screenLayout = drop (length screenLayoutPrefix) choosed
                 layoutFile = ".screenlayout/" ++ screenLayout
             loggedSpawn layoutFile
+        | webPrefix |- choosed -> do
+            let web = drop (length webPrefix) choosed
+            case lookup web webAppMap of
+                Just (substring, url) -> openWebPage substring url
+                _ -> return ()
+        | True -> spawn $ unwords ["notify-send 'Pattern Match Failure' '", choosed, ","]
   where
-    args = [sWidth, sHeight, foregroundColor, themeColor]
     terminalRun = nextMatchOrDo Forward (("st"==) <$> title) $ loggedSpawn "exec st -f 'Monofur for Powerline:size=19'"
     hangups = nextTitleMatchOrSpawn "hangups" "exec st -f 'Monofur for Powerline:size=19' -e hangups"
     getXselection = runProcessWithInput "xclip" ["-o", "-selection"] ""
-    commandRun "jira" = openWebPage "JIRA" "jira.balabit"
-    commandRun "mail" = openWebPage "Inbox" "inbox.google.com"
-    commandRun "inbox" = openWebPage "Inbox" "inbox.google.com"
-    commandRun "calendar" = openWebPage "Calendar" "calendar.google.com"
-    commandRun "hangups" = hangups
-    commandRun "hangout" = hangups
-    commandRun "jenkins" = openWebPage "Jenkins" "jenkins.bsp.balabit"
+    commandRun c | c `elem` ["hangups", "hangout"] = hangups
     commandRun "pycharm" = nextTitleMatchOrSpawn "PyCharm" "exec pycharm"
-    commandRun "term" = terminalRun
-    commandRun "st" = terminalRun
+    commandRun c | c `elem` ["term", "st"] = terminalRun
     commandRun "google" = do
         selection <- getXselection
         let onelined = unwords $ lines selection
-        loggedSpawn $ "xdg-open https://www.google.com/search?q=['" ++ onelined ++ "']"
+        loggedSpawn $ "xdg-open https://www.google.com/search?q='" ++ onelined ++ "'"
     commandRun "openUrl" = do
         selection <- getXselection
         loggedSpawn $ "xdg-open " ++ selection
@@ -98,16 +98,23 @@ customCommandMenu = do
     commandRun "xmonad config" = nextTitleMatchOrSpawn ".xmonad" "exec atom .xmonad"
     commandRun "review" = nextTitleMatchOrSpawn "review.balabit" "google-chrome  --incognito --app=https://review.balabit/#/q/project:bsp/bsp+status:open"
     commandRun "network" = loggedSpawn "nmcli_dmenu"
-    commandRun "wifi" = do
-        switch <- menuArgs dmenuBin args ["off", "on"]
-        loggedSpawn $ "notify-send 'Wifi' `nmcli radio wifi " ++ switch ++ "`"
+    commandRun ('w':'i':'f':'i':' ':switch)= wifi switch
     commandRun _ = return ()
+    wifi switch = loggedSpawn $ "notify-send 'Wifi' `nmcli radio wifi " ++ switch ++ "`"
+
+webAppMap :: [(String, (String, String))]
+webAppMap =
+    [ ("jenkins", ("Jenkins", "jenkins.bsp.balabit"))
+    , ("mail", ("Inbox", "inbox.google.com"))
+    , ("inbox", ("Inbox", "inbox.google.com"))
+    , ("calendar", ("Calendar", "calendar.google.com"))
+    , ("jira", ("Jira", "jira.balabit"))
+    ]
 
 loggedSpawn :: String -> X ()
 loggedSpawn c = spawn $ "echo 'Spawn: "++c++ "'>> .logs/xmonad.log; " ++ c
 
-dmenuRunFile, dmenuRun :: String
-dmenuRunFile = dmenuBin
+dmenuRun :: String
 dmenuRun = ".xmonad/assets/bin/dmenu-run.sh '"++sWidth ++"' '"++sHeight ++"' '"++foregroundColor++"' '"++themeColor++"'"
 
 dmenuRunMenu :: X ()
@@ -124,3 +131,5 @@ windowMap = do
   where
     keyValuePair w = flip (,) w . map toLower .show <$> getName w
 
+dmenuBin = "yeganesh"
+dmenuArgs = words "-p xmonadmenu -f -- -b -h 24 -l 9 -x 575 -y 342 -w 450 -nb #000000 -sb #0783c0 -dim 0.65"
