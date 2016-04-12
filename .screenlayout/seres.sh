88c1482466022baa53da42774191d34d580ace78
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2016-04-12 10:35:59 +0200

    update screen layouts with a workaround
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

---
+++ .screenlayout/seres.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1600x900 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --crtc 1 --mode 1920x1080 --pos 1600x0 --rotate normal --output DP1-1 --crtc 2 --mode 1920x1080 --pos 3520x0 --rotate normal
