fc6bb766f1b4dd2fa337582fcc4541874d2ae465
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2016-04-19 08:57:51 +0200

    change desk
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

--- .screenlayout/work.sh
+++ .screenlayout/work.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 1600x900 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP1-3 --off --output DP1-2 --crtc 2 --mode 1920x1080 --pos 1600x0 --rotate normal --output DP1-1 --crtc 1 --mode 1920x1080 --pos 3520x0 --rotate normal
+xrandr --output eDP1 --mode 1600x900 --pos 0x300 --rotate normal --output DP1-3 --mode 1920x1080 --pos 3520x0 --rotate normal --crtc 1 --output DP1-1 --crtc 2 --mode 1920x1200 --pos 1600x0 --rotate normal
