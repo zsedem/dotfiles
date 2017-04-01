355cd27d5fa5623a46bcf36768af2d96ba27b220
Zsigmond Adam Oliver <adam.zsigmond@balabit.com>
2017-04-01 23:42:28 +0200

    remove .xmonad and ssh-agent
    Signed-off-by: Zsigmond Adam Oliver <adam.zsigmond@balabit.com>

--- .xmonad/lib/XMonad/Config/ZsEdem.hs
+++
@@ -1,8 +0,0 @@
-module XMonad.Config.ZsEdem(
-    module All
-) where
-
-import XMonad.Config.ZsEdem.Core as All
-import XMonad.Config.ZsEdem.Keys as All
-import XMonad.Config.ZsEdem.UrgencyHook as All
-import XMonad.Config.ZsEdem.Startup as All
